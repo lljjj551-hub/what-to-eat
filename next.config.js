d745a097a1c1890af8a090d1b690f7edf9577925
User <user@example.com>
2026-05-03 16:51:56 +0800

    Multi-platform deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/what-to-eat",
   images: { unoptimized: true },
 };
-
 module.exports = nextConfig;
